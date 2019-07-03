7e198320e4b919af345f3a08961b397e7c4a3628
Danny Tarn <marzique@Dannys-MBP.lan>
2019-07-03 16:28:21 +0300

    new rotation with movement

--- static/js/main.js
+++ static/js/main.js
@@ -21,12 +21,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
     $('#slider').mousemove(function(event) {
         if (!$("#title_main").hasClass("opened")){
+            var trans = 0;
+            xPercent = event.pageX / $( document ).width() * 100 * 4;
+            trans = (xPercent - 50)/4;
+            backtrans = -(xPercent - 50)/4
             var angle_big = Math.atan2(event.pageY - centerY_big, event.pageX - centerX_big) * 180 / Math.PI;
             var angle_small = Math.atan2(event.pageY - centerY_small, event.pageX - centerX_small) * 180 / Math.PI;
-    
+            console.log(angle_big)
+        
             // console.log(Math.abs(angle_big));
-            $(".big").rotate(angle_big - 360 - 45);
-            $(".small").rotate(angle_small - 360 - 45);   
+            // $(".big").rotate(angle_big - 360 - 45);
+            var angle_big_full = angle_big < 0 ? angle_big : -180 - (180 - angle_big);
+            var angle_small_full = angle_small < 0 ? angle_small : -180 - (180 - angle_small);
+
+            $(".big").css({"-webkit-transform":"translate(" + backtrans +"px)" + 
+                            "rotate(" + angle_big_full + "deg)"});
+            $(".small").css({"-webkit-transform":"translate(" + trans +"px)" + 
+                            "rotate(" + angle_small_full + "deg)"});
         }
     });
 
