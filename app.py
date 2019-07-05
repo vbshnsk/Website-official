c3eb8b78044ef4c1c620ceeca25d12c4d53868b0
marzique <marzique@gmail.com>
2019-07-05 23:51:44 +0300

    removed unneded href of contact

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("main.html")
 
+@app.route('/egg')
+def egg():
+    return render_template("main.html")
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
